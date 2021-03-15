c18c3653df7894640d7b316f527531ec59e8dce8
FelipeSimoes1 <ssimoes.felipe@gmail.com>
2021-03-15 14:35:33 -0300

    Incluindo função listar por ID

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,15 +1,16 @@
 package one.digitalinnovation.personapi.service;
 
-import lombok.RequiredArgsConstructor;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -35,5 +36,11 @@ public class PersonService {
                 .map(personMapper::toDTO)
                 .collect(Collectors.toList());
 
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(()-> new PersonNotFoundException(id));
+        return personMapper.toDTO(person);
     }
 }
